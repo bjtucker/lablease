ffbf13cd8760f875a410faf8c22d28fa6534aadf
Ben Tucker <ben@brickish.us>
2020-03-13 15:14:05 -0500

    Tidy tests and fix libraries

--- aws.py
+++ aws.py
@@ -1,5 +1,4 @@
-import boto3.ec2
-import boto3.client
+import boto3
 import datetime
 import json
 import logging
@@ -11,14 +10,14 @@ sns = boto3.client('sns')
 phone_number = '16127502017'  # change it to your phone number
 
 def terminate_instance(instance):
-    result += "terminating:" + instance.id +" "+ instance.state['Name'] +"  "
+    message = "terminating:" + instance.id +" "+ instance.state['Name'] +"  "
     instance.terminate()
-    return result
+    return message
 
 def lambda_handler(event, context):
     logger.info('Received event: ' + json.dumps(event))
-    killtime = datetime.timedelta(minutes=150)
-    result = "foo"
+    killtime = datetime.timedelta(minutes=180)
+    result = ""
     ec2 = boto3.resource('ec2', region_name='us-east-1')
 #   volumes = ec2.volumes.all()
 #   volume_ids = [v.id for v in volumes]
@@ -30,10 +29,10 @@ def lambda_handler(event, context):
         if datetime_now - instance.launch_time > killtime:
             result += terminate_instance(instance)
 
-    if result is not ""
-        message=json.dumps(result) 
-        sns.publish(PhoneNumber=phone_number, Message=message)
+    if result is not "":
+        sns.publish(PhoneNumber=phone_number, Message="lablease Deleted instances:" + result)
         logger.info('SMS has been sent to ' + phone_number)
 
     return result
+
 
