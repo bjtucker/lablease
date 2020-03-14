07a1a9ce70f544679b60e9e5d66eb5e67bdf72cc
Ben Tucker <ben@brickish.us>
2020-03-14 18:36:19 -0500

    Tidy with black

--- aws.py
+++ aws.py
@@ -6,21 +6,24 @@ import logging
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-sns = boto3.client('sns')
-phone_number = '16127502017'  # change it to your phone number
+sns = boto3.client("sns")
+phone_number = "16127502017"  # change it to your phone number
 default_lease_minutes = 180
 
+
 def terminate_instance(instance):
-    message = "terminating:" + instance.id +" "+ instance.state['Name'] +"  "
+    message = "terminating:" + instance.id + " " + instance.state["Name"] + "  "
     instance.terminate()
     return message
 
+
 def get_lease(instance):
-    tags_dict = {t['Key']: t['Value'] for t in instance.tags}
+    tags_dict = {t["Key"]: t["Value"] for t in instance.tags}
     try:
-        lease_minutes = tags_dict['lease_minutes']
+        lease_minutes = tags_dict["lease_minutes"]
     except KeyError:
         lease_minutes = default_lease_minutes
+
 
 def get_expired_instances(ec2):
     expired_instances = []
@@ -34,18 +37,21 @@ def get_expired_instances(ec2):
             expired_instances.append(instance)
     return expired_instances
 
+
 def lambda_handler(event, context):
-    logger.info('Received event: ' + json.dumps(event))
+    logger.info("Received event: " + json.dumps(event))
     result = ""
-    regions = ['us-east-1','us-east-2']
+    regions = ["us-east-1", "us-east-2"]
     for region in regions:
-        ec2 = boto3.resource('ec2', region_name=region)
+        ec2 = boto3.resource("ec2", region_name=region)
         expired_instances = get_expired_instances(ec2)
         for instance in expired_instances:
             result += terminate_instance(instance)
 
     if result is not "":
-        sns.publish(PhoneNumber=phone_number, Message="lablease Deleted instances:" + result)
-        logger.info('SMS has been sent to ' + phone_number)
+        sns.publish(
+            PhoneNumber=phone_number, Message="lablease Deleted instances:" + result
+        )
+        logger.info("SMS has been sent to " + phone_number)
 
     return result
